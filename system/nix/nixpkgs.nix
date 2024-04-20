ee54c080ce7bc7272e3ea148a3428bcb53ddaf2b
Amadej Kastelic <amadejkastelic7@gmail.com>
2024-04-20 19:03:17 +0200

    vesktop git version

--- system/nix/nixpkgs.nix
+++ system/nix/nixpkgs.nix
@@ -1,4 +1,8 @@
-{self, ...}: {
+{
+  self,
+  lib,
+  ...
+}: {
   nixpkgs = {
     config.allowUnfree = true;
     config.permittedInsecurePackages = [
@@ -12,6 +16,20 @@
           // {
             colors = import "${self}/lib/colors" prev.lib;
           };
+      })
+      (final: prev: {
+        vesktop = prev.vesktop.overrideAttrs (old: {
+          version = "main";
+          src = prev.fetchFromGitHub {
+            owner = "Vencord";
+            repo = "Vesktop";
+            rev = "${old.version}";
+            hash = "sha256-s3ndHHN8mqbzL40hMDXXDl+VV9pOk4XfnaVCaQvFFsg=";
+          };
+          pnpmDeps = old.pnpmDeps.overrideAttrs (x: {
+            outputHash = "sha256-6ezEBeYmK5va3gCh00YnJzZ77V/Ql7A3l/+csohkz68=";
+          });
+        });
       })
     ];
   };
