95df2378174e3d155b993fc6c5c338336cfc5e7b
Amadej Kastelic <amadejkastelic7@gmail.com>
2025-04-21 19:21:43 +0200

    cleanup

--- home/editors/neovim/default.nix
+++ home/editors/neovim/default.nix
@@ -1,4 +1,4 @@
-{pkgs, ...}: {
+{
   catppuccin.nvim.enable = true;
 
   programs.neovim = {
@@ -6,38 +6,5 @@
 
     vimAlias = true;
     viAlias = true;
-    vimdiffAlias = true;
-
-    plugins = with pkgs.vimPlugins; [
-      catppuccin-nvim
-      cmp-buffer
-      cmp-nvim-lsp
-      cmp-path
-      cmp-spell
-      cmp-treesitter
-      cmp-vsnip
-      friendly-snippets
-      gitsigns-nvim
-      lightline-vim
-      lspkind-nvim
-      neogit
-      null-ls-nvim
-      nvim-autopairs
-      nvim-cmp
-      nvim-colorizer-lua
-      nvim-lspconfig
-      nvim-tree-lua
-      (nvim-treesitter.withPlugins (_: pkgs.tree-sitter.allGrammars))
-      plenary-nvim
-      rainbow-delimiters-nvim
-      telescope-fzy-native-nvim
-      telescope-nvim
-      vim-floaterm
-      vim-sneak
-      vim-vsnip
-      which-key-nvim
-    ];
-
-    extraPackages = with pkgs; [gcc ripgrep fd];
   };
 }
