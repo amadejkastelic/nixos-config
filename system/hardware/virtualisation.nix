5b7adbc78f6ea971a6eb2a925926da5a453f608f
Amadej Kastelic <amadejkastelic7@gmail.com>
2025-05-03 11:40:21 +0200

    virtualisation: Add dns entries to docker

--- system/hardware/virtualisation.nix
+++ system/hardware/virtualisation.nix
@@ -19,6 +19,12 @@
       enable = true;
       package = pkgs.docker;
       setSocketVariable = true;
+      daemon.settings = {
+        dns = [
+          "1.1.1.1"
+          "8.8.8.8"
+        ];
+      };
     };
 
     libvirtd.enable = true;
