8b9c0e78c7350b0a452ff334e83a89d9ab2eab97
Amadej Kastelic <amadejkastelic7@gmail.com>
2025-05-14 19:33:08 +0200

    flake.lock: update

--- flake.nix
+++ flake.nix
@@ -74,7 +74,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    hyprland.url = "github:gulafaran/hyprland?ref=gl2";
+    hyprland.url = "github:hyprwm/hyprland";
 
     hypridle = {
       url = "github:hyprwm/hypridle";
