8d164ad3b0578b8125fa3e61b9d4ee05349a2478
Amadej Kastelic <amadejkastelic7@gmail.com>
2025-07-05 13:09:16 +0200

    home/mangohud: disable catppuccin, which overrides config https://github.com/catppuccin/nix/pull/599

--- home/programs/games/mangohud.nix
+++ home/programs/games/mangohud.nix
@@ -2,11 +2,15 @@
   programs.mangohud = {
     enable = true;
     settings = {
+      full = false;
       cpu_temp = true;
       gpu_temp = true;
-      vram = true;
+      vram = false;
       frame_timing = false;
       toggle_hud = "Shift_L+F11";
+      position = "top-right";
     };
   };
+
+  catppuccin.mangohud.enable = false;
 }
