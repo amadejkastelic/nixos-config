e55812601a418eac38fb401bba46a74edbc44f5e
Amadej Kastelic <amadejkastelic7@gmail.com>
2025-07-05 12:56:28 +0200

    terminal/ghostty: fix regression

--- home/terminal/emulators/ghostty.nix
+++ home/terminal/emulators/ghostty.nix
@@ -1,13 +1,23 @@
+{ pkgs, ... }:
 {
   programs.ghostty = {
     enable = true;
+
+    package = pkgs.ghostty.overrideAttrs (_: {
+      preBuild = ''
+        shopt -s globstar
+        sed -i 's/^const xev = @import("xev");$/const xev = @import("xev").Epoll;/' **/*.zig
+        shopt -u globstar
+      '';
+    });
+
     enableZshIntegration = true;
 
     clearDefaultKeybinds = false;
 
     settings = {
       font-family = "JetBrainsMono Nerd Font Mono";
-      font-size = 13;
+      font-size = 12;
 
       background-opacity = "0.9";
 
