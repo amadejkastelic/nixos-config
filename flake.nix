e4801409fe9827af041f3b08dcc5b1ce48770419
Amadej Kastelic <amadejkastelic7@gmail.com>
2025-07-10 21:50:12 +0200

    chore: fix hypr-dynamic-cursors

--- flake.nix
+++ flake.nix
@@ -117,7 +117,7 @@
     };
 
     hypr-dynamic-cursors = {
-      url = "github:VirtCode/hypr-dynamic-cursors";
+      url = "github:amadejkastelic/hypr-dynamic-cursors?ref=fix-build";
       inputs.hyprland.follows = "hyprland";
     };
 
