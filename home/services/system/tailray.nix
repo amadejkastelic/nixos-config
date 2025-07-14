9ed04d68a6df86bfd261f5ffae4c0dc561629d6a
Amadej Kastelic <amadejkastelic7@gmail.com>
2025-07-14 18:08:23 +0200

    flake.lock: update

--- home/services/system/tailray.nix
+++ home/services/system/tailray.nix
@@ -1,12 +1,6 @@
-{
-  lib,
-  inputs,
-  ...
-}:
+{ inputs, ... }:
 {
   imports = [ inputs.tailray.homeManagerModules.default ];
 
   services.tailray.enable = true;
-
-  systemd.user.services.tailray.Unit.After = lib.mkForce "graphical-session.target";
 }
