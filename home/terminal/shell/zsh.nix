b901579fe424e66eee128d6371d15e7292faa57e
Amadej Kastelic <amadejkastelic7@gmail.com>
2025-07-20 17:15:42 +0200

    home/programs/zsh: updates

--- home/terminal/shell/zsh.nix
+++ home/terminal/shell/zsh.nix
@@ -5,31 +5,21 @@
   ...
 }:
 {
-  home.packages = with pkgs; [
-    fzf
-  ];
+  home.packages = [ pkgs.fzf ];
 
   programs.zsh = {
     enable = true;
     autosuggestion = {
       enable = true;
     };
-    oh-my-zsh = {
-      enable = true;
-      plugins = [
-        "git"
-        "fzf"
-      ];
-    };
     autocd = true;
     dirHashes = {
       dl = "$HOME/Downloads";
       docs = "$HOME/Documents";
-      code = "$HOME/Documents/code";
-      dots = "$HOME/Documents/code/dotfiles";
+      dots = "$HOME/Documents/dotfiles";
       pics = "$HOME/Pictures";
       vids = "$HOME/Videos";
-      nixpkgs = "$HOME/Documents/code/git/nixpkgs";
+      nixpkgs = "$HOME/Documents/nixpkgs";
     };
     dotDir = ".config/zsh";
     history = {
@@ -38,52 +28,6 @@
     };
 
     syntaxHighlighting.enable = true;
-
-    /*
-        initExtra = ''
-        # search history based on what's typed in the prompt
-        autoload -U history-search-end
-        zle -N history-beginning-search-backward-end history-search-end
-        zle -N history-beginning-search-forward-end history-search-end
-        bindkey "^[OA" history-beginning-search-backward-end
-        bindkey "^[OB" history-beginning-search-forward-end
-
-        # C-right / C-left for word skips
-        bindkey "^[[1;5C" forward-word
-        bindkey "^[[1;5D" backward-word
-
-        # C-Backspace / C-Delete for word deletions
-        bindkey "^[[3;5~" forward-kill-word
-        bindkey "^H" backward-kill-word
-
-        # Home/End
-        bindkey "^[[OH" beginning-of-line
-        bindkey "^[[OF" end-of-line
-
-        # open commands in $EDITOR with C-e
-        autoload -z edit-command-line
-        zle -N edit-command-line
-        bindkey "^e" edit-command-line
-
-        # case insensitive tab completion
-        zstyle ':completion:*' completer _complete _ignored _approximate
-        zstyle ':completion:*' list-prompt %SAt %p: Hit TAB for more, or the character to insert%s
-        zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}'
-        zstyle ':completion:*' menu select
-        zstyle ':completion:*' select-prompt %SScrolling active: current selection at %p%s
-        zstyle ':completion:*' verbose true
-
-        # use cache for completions
-        zstyle ':completion:*' use-cache on
-        zstyle ':completion:*' cache-path "$XDG_CACHE_HOME/zsh/.zcompcache"
-        _comp_options+=(globdots)
-
-        ${lib.optionalString config.services.gpg-agent.enable ''
-          gnupg_path=$(ls $XDG_RUNTIME_DIR/gnupg)
-          export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/gnupg/$gnupg_path/S.gpg-agent.ssh"
-        ''}
-      '';
-    */
 
     shellAliases = {
       grep = "grep --color";
@@ -101,6 +45,47 @@
     shellGlobalAliases = {
       eza = "eza --icons --git";
     };
+
+    initContent = ''
+      autoload -U history-search-end
+      zle -N history-beginning-search-backward-end history-search-end
+      zle -N history-beginning-search-forward-end history-search-end
+      bindkey "^[OA" history-beginning-search-backward-end
+      bindkey "^[OB" history-beginning-search-forward-end
+
+      # C-right / C-left for word skips
+      bindkey "^[[1;5C" forward-word
+      bindkey "^[[1;5D" backward-word
+
+      # C-Backspace / C-Delete for word deletions
+      bindkey "^[[3;5~" forward-kill-word
+      bindkey "^H" backward-kill-word
+
+      # Home/End
+      bindkey "^[[OH" beginning-of-line
+      bindkey "^[[OF" end-of-line
+
+      # open commands in $EDITOR with C-e
+      autoload -z edit-command-line
+      zle -N edit-command-line
+      bindkey "^e" edit-command-line
+
+      # case insensitive tab completion
+      zstyle ':completion:*' completer _complete _ignored _approximate
+      zstyle ':completion:*' list-prompt %SAt %p: Hit TAB for more, or the character to insert%s
+      zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}'
+      zstyle ':completion:*' menu select
+      zstyle ':completion:*' select-prompt %SScrolling active: current selection at %p%s
+      zstyle ':completion:*' verbose true
+
+      # use cache for completions
+      zstyle ':completion:*' use-cache on
+      zstyle ':completion:*' cache-path "$XDG_CACHE_HOME/zsh/.zcompcache"
+      _comp_options+=(globdots)
+
+      # Set up fzf key bindings and fuzzy completion
+      source <(fzf --zsh)
+    '';
   };
 
   catppuccin.zsh-syntax-highlighting.enable = true;
