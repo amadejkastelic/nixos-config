a42c87046366529bb95f648cf2beb98be25a8fb2
Amadej Kastelic <amadejkastelic7@gmail.com>
2025-07-22 19:15:12 +0200

    pkgs/cider: cleanup

--- pkgs/cider/default.nix
+++ pkgs/cider/default.nix
@@ -1,28 +1,19 @@
 {
-  lib,
   stdenv,
   fetchurl,
   dpkg,
   autoPatchelfHook,
   makeWrapper,
+
   # Required dependencies for autoPatchelfHook
   alsa-lib,
-  at-spi2-atk,
-  cairo,
-  cups,
-  expat,
-  gcc,
   gtk3,
-  libxkbcommon,
-  mesa,
+  libgbm,
   nspr,
   nss,
-  pango,
-  systemd,
-  xorg,
 }:
 stdenv.mkDerivation rec {
-  pname = "cider";
+  pname = "cider-2";
   version = "3.0.0";
 
   src = fetchurl {
@@ -38,30 +29,15 @@ stdenv.mkDerivation rec {
 
   buildInputs = [
     alsa-lib
-    at-spi2-atk
-    cairo
-    cups
-    expat
-    gcc.cc.lib
     gtk3
-    libxkbcommon
-    mesa
+    libgbm
     nspr
     nss
-    pango
-    systemd
-    xorg.libX11
-    xorg.libXcomposite
-    xorg.libXdamage
-    xorg.libXext
-    xorg.libXfixes
-    xorg.libXrandr
-    xorg.libxcb
   ];
 
   unpackPhase = ''
     runHook preUnpack
-    dpkg-deb --fsys-tarfile $src | tar x --no-same-owner --no-same-permissions
+    dpkg-deb --fsys-tarfile $src | tar --extract
     runHook postUnpack
   '';
 
@@ -72,33 +48,29 @@ stdenv.mkDerivation rec {
     cp -r usr/share/* $out/share/
     cp -r usr/lib/* $out/lib/
 
-    # Make the executable actually executable
     chmod +x $out/lib/cider/Cider
 
     runHook postInstall
   '';
 
   postFixup = ''
-    # Create wrapper script with Wayland, sandbox, and scaling flags (matching nixpkgs pattern)
-    makeWrapper $out/lib/cider/Cider $out/bin/cider \
+    makeWrapper $out/lib/cider/Cider $out/bin/${pname} \
       --add-flags "\$\{NIXOS_OZONE_WL:+\$\{WAYLAND_DISPLAY:+--ozone-platform-hint=auto --enable-features=WaylandWindowDecorations --enable-wayland-ime=true\}\}" \
       --add-flags "--no-sandbox --disable-gpu-sandbox"
 
-    # Fix desktop file
-    substituteInPlace $out/share/applications/cider.desktop \
+    mv $out/share/applications/cider.desktop $out/share/applications/${pname}.desktop
+    substituteInPlace $out/share/applications/${pname}.desktop \
       --replace-warn 'Exec=cider' 'Exec=${pname}' \
       --replace-warn 'Exec=/usr/lib/cider/Cider' 'Exec=${pname}'
 
-    # Install icon in proper location
-    mkdir -p $out/share/icons/hicolor/256x256/apps
-    cp $out/share/pixmaps/cider.png $out/share/icons/hicolor/256x256/apps/cider.png
+    install -Dm444 $out/share/pixmaps/cider.png \
+      $out/share/icons/hicolor/256x256/apps/cider.png
   '';
 
   meta = {
     description = "Powerful music player that allows you listen to your favorite tracks with style";
     homepage = "https://cider.sh";
-    maintainers = [ ];
+    mainProgram = "cider-2";
     platforms = [ "x86_64-linux" ];
-    mainProgram = "cider";
   };
 }
