5e664b8a660bf1e4fedafe72a6c8c840a49c93a9
Amadej Kastelic <amadejkastelic7@gmail.com>
2025-07-27 18:19:41 +0200

    home/programs/claude: add mcp config

--- home/terminal/programs/claude.nix
+++ home/terminal/programs/claude.nix
@@ -1,10 +1,39 @@
-{ pkgs, ... }:
 {
-  home.packages = [ pkgs.claude-code ];
+  lib,
+  pkgs,
+  self,
+  config,
+  ...
+}:
+let
+  mcp-nixos = self.packages.${pkgs.system}.mcp-nixos;
+in
+{
+  home.packages = [
+    mcp-nixos
+    (pkgs.claude-code.overrideAttrs (oa: {
+      postInstall = ''
+        wrapProgram $out/bin/claude \
+          --set DISABLE_AUTOUPDATER 1 \
+          --unset DEV_MODE \
+          --add-flags "--mcp-config ${config.home.file.".config/mcp.json".source}"
+      '';
+    }))
+  ];
 
-  # https://docs.anthropic.com/en/docs/claude-code/settings
-  home.file.".claude/settings.json".text = builtins.toJSON {
-    includeCoAuthoredBy = false;
-    enableAllProjectMcpServers = true;
+  home.file = {
+    # https://docs.anthropic.com/en/docs/claude-code/settings
+    ".claude/settings.json".text = builtins.toJSON {
+      includeCoAuthoredBy = false;
+      enableAllProjectMcpServers = true;
+    };
+
+    ".config/mcp.json".text = builtins.toJSON {
+      mcpServers = {
+        nixos = {
+          command = lib.getExe mcp-nixos;
+        };
+      };
+    };
   };
 }
