5e5b5ede9f6a7444910832c94b89de9f694029a1
Amadej Kastelic <amadejkastelic7@gmail.com>
2025-12-31 13:57:21 +0100

    home/terminal/git: clean up aliases

--- home/terminal/programs/git.nix
+++ home/terminal/programs/git.nix
@@ -32,24 +32,7 @@
       };
 
       aliases = {
-        a = "add";
-        b = "branch";
-        c = "commit";
-        ca = "commit --amend";
-        cm = "commit -m";
-        co = "checkout";
-        d = "diff";
-        ds = "diff --staged";
-        p = "push";
-        pf = "push --force-with-lease";
-        pl = "pull";
-        l = "log";
-        r = "rebase";
-        s = "status --short";
-        ss = "status";
-        forgor = "commit --amend --no-edit";
-        graph = "log --all --decorate --graph --oneline";
-        oops = "checkout --";
+        unstage = "reset";
       };
     };
 
