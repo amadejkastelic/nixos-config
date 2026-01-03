dd4c8513f10ac755a9f10b7e0926f1b06b659d62
Amadej Kastelic <amadejkastelic7@gmail.com>
2026-01-03 16:08:44 +0100

    home/services/hyprpaper: use nixpkgs version until hm catches up

--- home/services/wayland/hyprpaper.nix
+++ home/services/wayland/hyprpaper.nix
@@ -8,14 +8,9 @@
 {
   services.hyprpaper = {
     enable = true;
-    package = inputs.hyprpaper.packages.${pkgs.stdenv.hostPlatform.system}.default;
+    # package = inputs.hyprpaper.packages.${pkgs.stdenv.hostPlatform.system}.default;
 
     settings = {
-      wallpaper = lib.mkForce {
-        monitor = "DP-2";
-        path = toString config.stylix.image;
-        fit_mode = "cover";
-      };
       splash = false;
     };
   };
