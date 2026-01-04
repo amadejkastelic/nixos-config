5021f0015606cbef02878893ecfd473fa965dcfb
Amadej Kastelic <amadejkastelic7@gmail.com>
2026-01-04 15:08:09 +0100

    chore: vesktop overlay

--- system/nix/nixpkgs.nix
+++ system/nix/nixpkgs.nix
@@ -19,6 +19,28 @@
       inputs.nix-vscode-extensions.overlays.default
       inputs.firefox-addons.overlays.default
       inputs.cachyos-kernel.overlays.pinned
+
+      # https://nixpkgs-tracker.ocfox.me/?pr=476347
+      (final: prev: {
+        vesktop = prev.vesktop.overrideAttrs (old: {
+          preBuild = ''
+            cp -r ${prev.electron.dist} electron-dist
+            chmod -R u+w electron-dist
+          '';
+          buildPhase = ''
+            runHook preBuild
+
+            pnpm build
+            pnpm exec electron-builder \
+              --dir \
+              -c.asarUnpack="**/*.node" \
+              -c.electronDist="electron-dist" \
+              -c.electronVersion=${prev.electron.version}
+
+            runHook postBuild
+          '';
+        });
+      })
     ];
   };
 }
