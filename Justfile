bd2c04bc7c1d49ea1cdd96fc3bce6f7a88f12ebb
Amadej Kastelic <amadejkastelic7@gmail.com>
2026-03-01 13:12:32 +0100

    chore: chase nixpkgs

--- Justfile
+++ Justfile
@@ -3,6 +3,7 @@ host := `hostname`
 update:
     nix flake update --commit-lock-file
 
+# https://nixpkgs-tracker.ocfox.me/?pr=494720
 upgrade TARGET=host:
     #!/usr/bin/env bash
     if [ "{{TARGET}}" = "{{host}}" ]; then
--- system/nix/nixpkgs.nix
+++ system/nix/nixpkgs.nix
@@ -19,31 +19,6 @@
       inputs.nix-vscode-extensions.overlays.default
       inputs.firefox-addons.overlays.default
       inputs.cachyos-kernel.overlays.pinned
-      # https://nixpkgs-tracker.ocfox.me/?pr=493376
-      (final: prev: {
-        pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [
-          (python-final: python-prev: {
-            picosvg = python-prev.picosvg.overridePythonAttrs (oldAttrs: {
-              doCheck = false;
-            });
-          })
-        ];
-      })
-      # https://nixpkgs-tracker.ocfox.me/?pr=486948
-      (final: prev: {
-        mcp-nixos = (
-          prev.mcp-nixos.overrideAttrs (old: rec {
-            version = "2.2.0";
-            src = prev.fetchFromGitHub {
-              owner = "utensils";
-              repo = "mcp-nixos";
-              rev = "v${version}";
-              hash = "sha256-/3/MUCjUu4iQOEmgda61ztO2d6e/HPpjsF9Z7hTWYMc=";
-            };
-            nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ prev.python3Packages.pytest-cov ];
-          })
-        );
-      })
     ];
   };
 }
