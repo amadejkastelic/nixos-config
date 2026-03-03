0f80ec09d4fa59b8a8753c8102e4660d29172f16
Amadej Kastelic <amadejkastelic7@gmail.com>
2026-03-03 17:19:54 +0100

    home/programs/vicinae/extensions: disable bluetooth

--- home/programs/vicinae/default.nix
+++ home/programs/vicinae/default.nix
@@ -44,7 +44,7 @@
     };
 
     extensions = with inputs.vicinae-extensions.packages.${pkgs.stdenv.hostPlatform.system}; [
-      bluetooth
+      # bluetooth
       nix
       wifi-commander
     ];
