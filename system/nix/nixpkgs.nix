26bf206916f9c80b55d78ddbf0b9c5113a94c183
Amadej Kastelic <amadejkastelic7@gmail.com>
2026-04-30 19:35:09 +0200

    overlays: remove python3Packages.aioboto3

--- system/nix/nixpkgs.nix
+++ system/nix/nixpkgs.nix
@@ -19,13 +19,10 @@
       inputs.nix-vscode-extensions.overlays.default
       inputs.firefox-addons.overlays.default
       inputs.cachyos-kernel.overlays.pinned
-      # https://nixpkgs-tracker.ocfox.me/?pr=513680
+      # https://nixpkgs-tracker.ocfox.me/?pr=511658
       (final: prev: {
         pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [
           (python-final: python-prev: {
-            aioboto3 = python-prev.aioboto3.overridePythonAttrs (oldAttrs: {
-              doCheck = false;
-            });
             fastmcp = python-prev.fastmcp.overridePythonAttrs (oldAttrs: {
               doCheck = false;
             });
