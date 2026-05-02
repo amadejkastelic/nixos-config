7864128f1015cd60cb919a649c07d7cfb4b11830
Amadej Kastelic <amadejkastelic7@gmail.com>
2026-05-02 13:28:23 +0200

    chore: chase nixpkgs

--- system/nix/nixpkgs.nix
+++ system/nix/nixpkgs.nix
@@ -19,16 +19,6 @@
       inputs.nix-vscode-extensions.overlays.default
       inputs.firefox-addons.overlays.default
       inputs.cachyos-kernel.overlays.pinned
-      # https://nixpkgs-tracker.ocfox.me/?pr=511658
-      (final: prev: {
-        pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [
-          (python-final: python-prev: {
-            fastmcp = python-prev.fastmcp.overridePythonAttrs (oldAttrs: {
-              doCheck = false;
-            });
-          })
-        ];
-      })
     ];
   };
 }
