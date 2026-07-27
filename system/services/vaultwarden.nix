9bba29c315bd6d5d1610c5af51ed12d8d5c1a82a
Amadej Kastelic <amadejkastelic7@gmail.com>
2026-07-27 17:39:20 +0200

    Revert "system/services/vaultwarden: 1.36.0 -> 1.37.0"
    This reverts commit 555540111c9d52e40598971de8fe1e8ffc49519c.

--- system/services/vaultwarden.nix
+++ system/services/vaultwarden.nix
@@ -1,57 +1,8 @@
-{ config, pkgs, ... }:
-let
-  # https://github.com/NixOS/nixpkgs/pull/545340
-  vaultwardenSrc = pkgs.fetchFromGitHub {
-    owner = "dani-garcia";
-    repo = "vaultwarden";
-    tag = "1.37.0";
-    hash = "sha256-7l9tIBCfk8DeQDtIoENnjGUzVWJM3aZxw6eA+YaktlM=";
-  };
-
-  vaultwarden = pkgs.vaultwarden.overrideAttrs {
-    version = "1.37.0";
-    src = vaultwardenSrc;
-
-    cargoHash = "sha256-sza4ZQz2+QJJJ03Upt6sGXAv+1VPImN2qZHXaTSALFQ=";
-    cargoDeps = pkgs.rustPlatform.fetchCargoVendor {
-      src = vaultwardenSrc;
-      hash = "sha256-sza4ZQz2+QJJJ03Upt6sGXAv+1VPImN2qZHXaTSALFQ=";
-    };
-  };
-
-  webVaultSrc = pkgs.fetchFromGitHub {
-    owner = "vaultwarden";
-    repo = "vw_web_builds";
-    tag = "v2026.6.4+0";
-    hash = "sha256-Uz0wPdhTVy2yOlKWAy5phr+30NmFaIPQQh5bsiWCDLA=";
-  };
-
-  webVault = pkgs.vaultwarden.webvault.overrideAttrs (
-    _: previousAttrs: {
-      version = "2026.6.4+0";
-      src = webVaultSrc;
-
-      postPatch = null;
-      npmDepsFetcherVersion = 3;
-      npmDepsHash = "sha256-PaDxqVsq00QIKDmhDhsEbKdM4QXfXn28PgpOyZjB60k=";
-      npmDeps = pkgs.fetchNpmDeps {
-        src = webVaultSrc;
-        hash = "sha256-PaDxqVsq00QIKDmhDhsEbKdM4QXfXn28PgpOyZjB60k=";
-        fetcherVersion = 3;
-      };
-      env = previousAttrs.env // {
-        NIX_NPM_FETCHER_VERSION = 3;
-      };
-    }
-  );
-in
+{ config, ... }:
 {
   homelab.subdomains = [ "vaultwarden" ];
   services.vaultwarden = {
     enable = true;
-    package = vaultwarden;
-    webVaultPackage = webVault;
-
     nginx = {
       enable = true;
       hostName = "vaultwarden.${config.homelab.domain}";
