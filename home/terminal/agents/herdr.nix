8a03fd623a409d4b41db158219bf1d2d1983b7ec
Amadej Kastelic <amadejkastelic7@gmail.com>
2026-08-09 16:50:59 +0200

    home/terminal/agents/herdr: update config

--- home/terminal/agents/herdr.nix
+++ home/terminal/agents/herdr.nix
@@ -3,8 +3,18 @@
     enable = true;
 
     settings = {
+      onboarding = false;
       terminal.default_shell = "zsh";
       theme.name = "catppuccin";
+
+      ui = {
+        agent_panel_sort = "priority";
+        hide_tab_bar_when_single_tab = true;
+        prompt_new_tab_name = false;
+        toast.delivery = "system";
+      };
+
+      update.version_check = false;
     };
   };
 }
