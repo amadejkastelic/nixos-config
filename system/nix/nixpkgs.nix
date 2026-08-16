6119413a11f1c58c56d80c38a3d8322ed0725032
Amadej Kastelic <amadejkastelic7@gmail.com>
2026-08-16 16:28:31 +0200

    chore: fix hash of python3Packages.nanoemoji

--- system/nix/nixpkgs.nix
+++ system/nix/nixpkgs.nix
@@ -16,6 +16,18 @@
       inputs.nix-vscode-extensions.overlays.default
       inputs.firefox-addons.overlays.default
       inputs.cachyos-kernel.overlays.pinned
+      # https://nixpkgs-tracker.ocfox.me/?pr=552075
+      (final: prev: {
+        pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [
+          (_: pyprev: {
+            nanoemoji = pyprev.nanoemoji.overrideAttrs (old: {
+              src = old.src.overrideAttrs (_: {
+                outputHash = "sha256-FysyKC01XBnRiur5RR9fcsTxQqE8x0JJHSoe3q6JtKc=";
+              });
+            });
+          })
+        ];
+      })
     ];
   };
 }
